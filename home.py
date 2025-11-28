e6569c7a7c719f5473da402da479a8c7b4be3daf
doritak <104630627-doritak@user.noreplay.github.com>
2025-11-28 11:43:18 +0100

    With interactived models

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from urllib.request import urlopen
-import json
+import pickle
 
 from copy import deepcopy
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
@@ -67,20 +67,70 @@ if "TotalCharges" in df.columns:
     df['TotalCharges'].fillna(df['TotalCharges'].mean(), inplace=True)
     
 df['Churn'] = df['Churn'].map({'Yes': 1, 'No': 0})
+########### guardar el data set para otra pagina ###########
+# Guardar DataFrame en pickle cuando se presione el botón
+if st.button("Save Dataset"):
+    with open("data/dataset.pkl", "wb") as f:
+        pickle.dump(df, f)
+    
+    st.success("Dataset saved successfully.")
+    st.write("Now you must go to Models page...")
+    # Cambio de 'pantalla' para Streamlit 1.24.1
+    st.session_state.pages = "Models"
+    st.experimental_rerun()
+
 
 ######### Dataset Overview #########
+unique = {}
+type_counts = {}
+nan_counts = {}
+
+for col in df.columns:
+    unique[col] = df[col].nunique()
+    type_counts[col] = df[col].dtype
+    nan_counts[col] = df[col].isna().sum()
+
+df_unique = pd.DataFrame({
+    "n_unique" : unique,
+    "dtype" : type_counts, 
+    "n_nan": nan_counts}
+)
+
 st.subheader("Dataset Overview")
 st.write("The dataset contains information about telecom customers and whether they have churned or not.")
 st.dataframe(df)
 st.write("Dataset shape:", df.shape)
-st.write("Columns:", columns)
+st.write(df_unique)
 
 ######### EDA #########
 st.subheader("Exploratory Data Analysis (EDA)")
 left_col,  right_col = st.columns(2)
 
 with left_col:
+    
     st.write("Churn distribution:")
+     # Contar churn
+     
+    # churn_df = (
+    #     df["Churn"]
+    #     .value_counts()
+    #     .rename_axis("Churn")         # nombre de la columna de categorías
+    #     .reset_index(name="count_value")    # nombre de la columna de conteos
+    # )
+    # churn_df["Churn"] = churn_df["Churn"].map({0: "No Churn", 1: "Churn"})
+    # st.write(churn_df)
+    # st.write(churn_df.columns)
+
+    # fig1 = px.pie(
+    #     churn_df,
+    #     names="Churn",     # columna con las etiquetas ("No Churn", "Churn")
+    #     values="count_value",    # columna con los valores (4399, 1587, etc.)
+    #     title="Churn Distribution",
+    # )
+    # fig1.update_traces(textposition="inside", textinfo="percent")
+    # st.plotly_chart(fig1, use_container_width=True)
+
+    
     churn_counts = df['Churn'].value_counts()
     fig1, ax1 = plt.subplots()
     ax1.pie(churn_counts, labels=churn_counts.index, autopct='%1.1f%%', startangle=90)
@@ -88,7 +138,7 @@ with left_col:
 
     ax1.axis('equal')
     st.pyplot(fig1)
-
+    
 with right_col:
     contract_churn = df.groupby('Contract')['Churn'].value_counts(normalize=True).unstack()
     fig2 = px.bar(contract_churn, barmode='group', title='Churn Rate by Contract Type')
@@ -97,6 +147,10 @@ with right_col:
 cat_Columns = df.select_dtypes(include=["object"]).columns.tolist()
 num_Columns = df.select_dtypes(include=["int64", "float64"]).columns.tolist()
 
+st.write("### Categorical Features vs Churn")
+st.write(f"The categorical features in the dataset are: {', '.join(cat_Columns)}")
+st.write("These stacked bar charts show how each categorical feature is distributed across Churn and Non-Churn customers.")
+st.write("Each bar represents the percentage breakdown within each churn group, adding up to 100%.")
 cols_per_row = 3
 
 for i, col in enumerate(cat_Columns):
@@ -114,21 +168,32 @@ for i, col in enumerate(cat_Columns):
     ax.set_title(f"Percentual Distribution '{col}' by Churn")
     ax.legend(title=col, bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
+    # mandamos el gráfico a la columna que toca (0,1,2)
+    with st_cols[i % cols_per_row]:
+        st.pyplot(fig)
+    # mandamos el gráfico a la columna que toca (0,1,2)
+num_Columns_only = [col for col in num_Columns if col != "Churn"]    
+st.write("### Numerical Features vs Churn")
+st.write(f"The numercial features in the dataset are: {', '.join(num_Columns_only)}")
+st.write("Box plots showing the distribution of numerical features based on churn status.")
+
+for i, col in enumerate(num_Columns_only):
+    # cada 3 gráficos creo una nueva fila de columnas
+    if i % cols_per_row == 0:
+        st_cols = st.columns(cols_per_row)
+
+    # figura individual
+    fig, ax = plt.subplots(figsize=(4, 4))
+    df.boxplot(column=col, by='Churn', ax=ax)
+
+    ax.set_title(f"Box Plot of '{col}' by Churn")
+    ax.set_xlabel("Churn")
+    ax.set_ylabel(col)
+    plt.suptitle("")  # eliminar el título automático de pandas
+    plt.tight_layout()
 
     # mandamos el gráfico a la columna que toca (0,1,2)
     with st_cols[i % cols_per_row]:
         st.pyplot(fig)
 
 
-# for col in cat_Columns:
-#     table = pd.crosstab(df["Churn"], df[col], normalize='index') * 100
-#     print(f"\n{col}:")
-#     print(table)
-#     plt.figure(figsize=(8, 6))
-#     table.plot(kind="bar", stacked=True)
-
-#     plt.ylabel("Porcent (%)")
-#     plt.title(f"Porcentual Distribution '{col}' por Churn")
-#     plt.legend(title=col, bbox_to_anchor=(1.05, 1), loc='upper left')
-#     plt.tight_layout()
-#     st.pyplot(plt)
--- pages/1_Models.py
+++ pages/1_Models.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from urllib.request import urlopen
-import json
+import pickle
+import datetime
 
 from copy import deepcopy
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
@@ -19,7 +20,7 @@ from sklearn.compose import make_column_selector as selector
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.svm import LinearSVC, SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from xgboost import XGBClassifier
@@ -27,6 +28,165 @@ from xgboost import XGBClassifier
 from sklearn.metrics import confusion_matrix, classification_report, f1_score
 from sklearn.model_selection import cross_val_score
 
+if "scores" not in st.session_state:
+    # Try to load previous scores from pickle (optional)
+    try:
+        with open("scores.pkl", "rb") as f:
+            st.session_state.scores = pickle.load(f)
+    except FileNotFoundError:
+        st.session_state.scores = []  # start empty list
+
 
 st.title("Models Page")
 st.write("This is the models page : Gradient Boosting.")
+
+try:
+    with open("data/dataset.pkl", "rb") as f:
+        df = pickle.load(f)
+
+    st.write("DataFrame loaded from pickle:")
+    st.dataframe(df)
+
+except FileNotFoundError:
+    st.error("The file dataset.pkl was not found. Please go back to the previous page.")
+    
+######### Data Preprocessing #########
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+num_features = X.select_dtypes(include=["int64", "float64"]).columns.tolist()
+cat_features = X.select_dtypes(include=["object"]).columns.tolist()
+
+categorical_transformer = Pipeline(steps=[
+    ("onehot", OneHotEncoder(sparse_output=False,handle_unknown="ignore"
+                             ).set_output(transform="pandas"))
+                                          ])
+numeric_transformer = Pipeline(steps=[
+    ("scaler", StandardScaler().set_output(transform="pandas"))
+                                      ])
+preprocessor = ColumnTransformer(transformers=[
+    ("num", numeric_transformer, num_features),
+    ("cat", categorical_transformer, cat_features)
+    ]).set_output(transform="pandas")
+
+preprocessor_new = ColumnTransformer(
+    transformers=[
+        ("num", "passthrough", num_features),
+        ("cat", categorical_transformer, cat_features),
+    ]
+)
+
+# scores = {}
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=47)
+
+st.write("Data Preprocessing completed. Ready for model training.")
+st.write(f"X_train shape: {X_train.shape}, X_test shape: {X_test.shape}")
+
+### Select the model
+models_options = {
+    "Gradient Boosting": GradientBoostingClassifier(),
+    "AdaBoost": AdaBoostClassifier(),
+    "Random Forest": RandomForestClassifier(),
+    "Decision Tree": DecisionTreeClassifier(),
+    "SVM": SVC(),
+    "XGBoost": XGBClassifier(use_label_encoder=False, eval_metric='logloss'),
+    "Logistic Regression": LogisticRegression(),
+    "K-Nearest Neighbors": KNeighborsClassifier(),
+    "Bernoulli Naive Bayes": BernoulliNB(),
+    
+}
+
+st.subheader("📌 Select a model to train")
+
+model_name  = st.selectbox(
+    "Available models:",
+    list(models_options.keys())
+)
+treeModels = ["Random Forest", "Decision Tree", "Gradient Boosting", "XGBoost", "AdaBoost"]
+
+if model_name in treeModels:
+    st.write("You have selected a tree-based model.")
+    rand_st = 42
+    model = models_options[model_name].set_params(random_state=rand_st)
+    pipeline_model = Pipeline(steps=[
+        ("preprocessor", preprocessor_new),
+        ("model", model)
+    ])
+    pipeline_model.fit(X_train, y_train)
+    y_pred = pipeline_model.predict(X_test)
+    f1 = f1_score(y_test, y_pred)
+    st.write(f"F1 Score for {model_name}: {f1:.4f}")
+    st.write("Classification Report:")
+    st.text(classification_report(y_test, y_pred))
+    # scores[model_name] = round(f1_score(y_test, y_pred, average='weighted'), 3)
+    
+    cm = confusion_matrix(y_test, y_pred)
+    
+    fig, ax = plt.subplots()
+    ax.imshow(cm, cmap=plt.cm.Blues)
+    ax.grid(False)
+    ax.xaxis.set(ticks=(0, 1), ticklabels=("Predicted No Churn", "Predicted Churn"))
+    ax.yaxis.set(ticks=(0, 1), ticklabels=("Actual No Churn", "Actual Churn"))
+    for i in range(2):
+        for j in range(2):
+            ax.text(j, i, cm[i, j], ha="center", va="center", color="red")  
+    ax.set_title(f'Confusion Matrix for {model_name}')
+    st.pyplot(fig)
+    # Save the F1 score and timestamp in session state
+    run_info = {
+    "model": model_name,
+    "f1": round(f1_score(y_test, y_pred, average='weighted'), 3),
+    "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    st.session_state.scores.append(run_info)
+
+else:
+    st.write("Selected model is not tree-based. ")
+    model = models_options[model_name]
+    pipeline_model = Pipeline(steps=[
+        ("preprocessor", preprocessor),
+        ("model", model)
+    ])
+    pipeline_model.fit(X_train, y_train)
+    y_pred = pipeline_model.predict(X_test)
+    f1 = f1_score(y_test, y_pred)
+    st.write(f"F1 Score for {model_name}: {f1:.4f}")
+    st.write("Classification Report:")
+    st.text(classification_report(y_test, y_pred))
+    # scores[model_name] = round(f1_score(y_test, y_pred, average='weighted'), 3)
+    cm = confusion_matrix(y_test, y_pred)
+    
+    fig, ax = plt.subplots()
+    ax.imshow(cm, cmap=plt.cm.Blues)
+    ax.grid(False)
+    ax.xaxis.set(ticks=(0, 1), ticklabels=("Predicted No Churn", "Predicted Churn"))
+    ax.yaxis.set(ticks=(0, 1), ticklabels=("Actual No Churn", "Actual Churn"))
+    for i in range(2):
+        for j in range(2):
+            ax.text(j, i, cm[i, j], ha="center", va="center", color="red")
+    ax.set_title(f'Confusion Matrix for {model_name}')
+    st.pyplot(fig)
+    
+    # Save the F1 score and timestamp in session state
+    run_info = {
+    "model": model_name,
+    "f1": round(f1_score(y_test, y_pred, average='weighted'), 3),
+    "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    st.session_state.scores.append(run_info)
+    
+# Save scores to pickle
+with open("scores.pkl", "wb") as f:
+    pickle.dump(st.session_state.scores, f)
+        
+st.write("Model training and evaluation completed.")
+
+
+st.write("F1 Scores of all trained models:")
+st.write(st.session_state.scores)
+
+#### guardar el modelo entrenado ####
+model_filename = f"data/{model_name.replace(' ', '_').lower()}_model.pkl"
+with open(model_filename, "wb") as f:
+    pickle.dump(pipeline_model, f)
+st.success(f"Trained model saved as {model_filename}")
